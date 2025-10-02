8d484a2d7ce376667d544179bd929b6bb936c139
PC-12 <>
2025-10-02 10:28:06 +0300

    /

--- app/src/main/java/com/example/boringgame/MainActivity.java
+++ app/src/main/java/com/example/boringgame/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.boringgame;
 
 import android.os.Bundle;
 import android.widget.Button;
+import android.widget.ImageView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,11 +11,15 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
-Button btnUp,btnDown,btnRight,btnLeft;
+
+private Button btnUp,btnDown,btnRight,btnLeft;
+private ImageView i;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
 
     }
 }
