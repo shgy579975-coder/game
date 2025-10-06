0ebe0312f7c2adf9cc3e4a8705177592665296f1
PC-12 <>
2025-10-06 14:55:04 +0300

    /

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,12 +4,12 @@ plugins {
 
 android {
     namespace = "com.example.boringgame"
-    compileSdk = 35
+    compileSdk = 36
 
     defaultConfig {
         applicationId = "com.example.boringgame"
         minSdk = 30
-        targetSdk = 35
+        targetSdk = 36
         versionCode = 1
         versionName = "1.0"
 
--- app/src/main/java/com/example/boringgame/MainActivity.java
+++ app/src/main/java/com/example/boringgame/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.boringgame;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 
@@ -18,6 +19,54 @@ private ImageView i;
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btnUp = findViewById(R.id.up);
+        btnDown = findViewById(R.id.Down);
+        btnRight = findViewById(R.id.Right);
+        btnLeft = findViewById(R.id.left);
+        i = findViewById(R.id.imageView);
+        btnDown.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                i.setY(i.getY()+30);
+                if(i.getY()<295)
+                    i.setImageResource(R.drawable.ppg1);
+                else
+                    i.setImageResource(R.drawable.ppgboy);
+            }
+        });
+        btnUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                i.setY(i.getY() - 15);
+                if (i.getY() < 37)
+                    i.setImageResource(R.drawable.ppgboy);
+                else
+                    i.setImageResource(R.drawable.ppg1);
+
+            }
+        });
+        btnRight.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                i.setX(i.getX() + 15);
+                if (i.getX() > 356)
+                    i.setImageResource(R.drawable.ppgboy);
+                else
+                    i.setImageResource(R.drawable.ppg1);
+            }
+            });
+        btnLeft.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                i.setX(i.getX()-15);
+                if(i.getX()<50)
+                    i.setImageResource(R.drawable.ppgboy);
+                else
+                    i.setImageResource(R.drawable.ppg1);
+            }
+        });
+
+
 
 
 
